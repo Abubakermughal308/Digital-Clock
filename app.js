4f29443c246fd6f08bb8096cb3a3d6643ca2b6f9
AbubakerMughal <abubakermughal7890@gmail.com>
2024-08-17 23:53:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,23 @@
-let data =[
-
-
-]
-function Addall(){
-  localStorage.setItem('object' , JSON.stringify(data))
-  tabledata = document.querySelector(".table_data")
-
-  var object = localStorage.getItem('object')
-  var objectdata = JSON.parse(object)
-  
+function clock() {
     
- 
-  var element = ""
-  objectdata.map(record =>(
-  
-    element += `
-    <tr >    
+    let monthName = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let date = new Date()
+    document.getElementById('date').innerHTML = (daysName[date.getDay()] + " " + date.getDate() + " " + monthName[date.getMonth()] + " " + date.getFullYear())
+    console.log(date);
 
-      <td>${record.Tasks}</td>
-      <td>
-      <button onclick = "edits(${record.id})" class = "edits" >Edit</button>
-      <button onclick = "Del(${record.id})" class = "delete"  >Delete</button>
-      </td>
-    </tr>
-    `
-  ))
-  
-  tabledata.innerHTML = element;
+    let h = date.getHours();
+    let min = date.getDate();
+    let sec = date.getSeconds();
+
+
+
+    h = h<10 ? "0"+h: h
+    min = min<10 ? "0"+min : min;
+    sec = sec<10 ? "0"+sec : sec;
+
+    document.getElementById("hours").innerHTML = h
+    document.getElementById("minutes").innerHTML = min
+    document.getElementById("seconds").innerHTML = sec
 }
-
-
-function create(){
-  document.querySelector(".Create_form").style.display = "inline-flex"
-  document.querySelector(".update_form").style.display = "none"
-  document.querySelector(".add_div").style.display = "none"
-}
-
-function add() {
-  
-  task = document.querySelector(".tasks").value
-  let newobj = {id: 3 , Tasks : task}
-  data.push(newobj)
-   document.querySelector(".Create_form").style.display = "none"
-  document.querySelector(".add_div").style.display = "block"
-  
-  Addall()
-}
-function edits(id){
-
-  document.querySelector(".update_form").style.display = "inline-flex"
-  document.querySelector(".Create_form").style.display = "none"
-  document.querySelector(".id").style.display = "none"
-
-  let obj = data.find(record => record.id === id)
-  document.querySelector(".Utasks").value = obj.Tasks
-  document.querySelector(".id").value = obj.id
-
-}
-
-
-function Del(id) {
-  data = data.filter(rec => rec.id !== id)
-  Addall()
-}
-
-
-function update() {
-  let id = parseInt(document.querySelector(".id").value);
-  let utask = document.querySelector(".Utasks").value;
-
-  let index = data.findIndex(rec => rec.id === id);
-
-  data[index] = {id , utask};
-  document.querySelector(".update_form").style.display = "none"
-
-  Addall()
-}
+let interval = setInterval(clock, 500)
